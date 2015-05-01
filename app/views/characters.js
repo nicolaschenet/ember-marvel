3c8b7c62c270b0180de7c3b8b09964f47e841fa3
Nicolas Chenet <nicolas@wisembly.com>
2015-05-01 18:17:40 +0200

    Characters | Deactivate loadmore on scroll

--- app/views/characters.js
+++ app/views/characters.js
@@ -2,19 +2,19 @@ import Ember from 'ember';
 
 export default Ember.View.extend({
   willInsertElement: function () {
-    Ember.$(document).on('scroll', Ember.$.proxy(this.onScroll, this));
+    // Ember.$(document).on('scroll', Ember.$.proxy(this.onScroll, this));
   },
 
   willDestroyElement: function () {
     Ember.$(document).off('scroll', Ember.$.proxy(this.onScroll, this));
   },
 
-  onScroll: function (event) {
+  onScroll: function () {
     var $loadmore = this.$('#loadmore');
     if (!$loadmore.length) {
       return;
     }
-    var shouldLoadMore = $loadmore.offset().top <= $(window).scrollTop() + $(window).height();
+    var shouldLoadMore = $loadmore.offset().top <= Ember.$(window).scrollTop() + Ember.$(window).height();
     this.get('controller').set('shouldLoadMore', shouldLoadMore);
   }
 });
