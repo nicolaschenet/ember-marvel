78042ae77788fa60681818548852c51071102883
Nicolas Chenet <nicolas@wisembly.com>
2015-05-01 03:24:41 +0200

    Characters | Load more on scroll

---
+++ app/views/characters.js
@@ -0,0 +1,17 @@
+import Ember from 'ember';
+
+export default Ember.View.extend({
+  willInsertElement: function () {
+    Ember.$(document).on('scroll', Ember.$.proxy(this.onScroll, this));
+  },
+
+  willDestroyElement: function () {
+    Ember.$(document).off('scroll', Ember.$.proxy(this.onScroll, this));
+  },
+
+  onScroll: function (event) {
+    var $loadmore = this.$('#loadmore'),
+        shouldLoadMore = $loadmore.offset().top <= $(window).scrollTop() + $(window).height();
+    this.get('controller').set('shouldLoadMore', shouldLoadMore);
+  }
+});
